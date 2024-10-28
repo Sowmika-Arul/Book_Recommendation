94d7ff39c15a590454203af2d38d5c196b5f95b2
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2024-10-28 16:06:32 +0530

    link used

--- new/src/components/Navbar.js
+++ new/src/components/Navbar.js
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <div className="logo">Book Recommendation</div>
-        <ul className="nav-links">
-          {/* <li><a href="/">Home</a></li> */}
-          <li><a href="/feedback">Reviews</a></li>
-          <li><a href="/recommendations">Recommendations</a></li>
-          <li><a href="/award">Top Pick</a></li>
-          <li><a href="/feedform">Testimonial</a></li>
-          <li><a href="/favorite">Favorites</a></li>
-          <li><a href="/bookreader">Book Reader</a></li>
-        </ul>
-        <div className="nav-icons">
-        <a href="/book"><i className="fas fa-search"></i></a>
-         <a href="/profile"> <i className="fas fa-user"></i></a>
-        </div>
-      </nav>
-    );
-  };
+  return (
+    <nav className="navbar">
+      <div className="logo">Book Recommendation</div>
+      <ul className="nav-links">
+        {/* <li><Link to="/">Home</Link></li> */}
+        <li><Link to="/feedback">Reviews</Link></li>
+        <li><Link to="/recommendations">Recommendations</Link></li>
+        <li><Link to="/award">Top Pick</Link></li>
+        <li><Link to="/feedform">Testimonial</Link></li>
+        <li><Link to="/favorite">Favorites</Link></li>
+        <li><Link to="/bookreader">Book Reader</Link></li>
+      </ul>
+      <div className="nav-icons">
+        <Link to="/book"><i className="fas fa-search"></i></Link>
+        <Link to="/profile"><i className="fas fa-user"></i></Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
